d7a9b212029a0c0fe31f25f3e5b54d0b6911ef2b
Stephen Huan <stephen03.huan@gmail.com>
2024-06-06 04:49:10 -0400

    build: add pyproject.toml

---
+++ src/inflow/__init__.py
@@ -0,0 +1,25 @@
+from .inflow import (
+    PREFIX,
+    Block,
+    State,
+    get_lines,
+    get_prefix,
+    main,
+    parse_lines,
+    parse_prefix,
+    process,
+    vardp,
+)
+
+__all__ = [
+    "PREFIX",
+    "Block",
+    "State",
+    "get_lines",
+    "get_prefix",
+    "main",
+    "parse_lines",
+    "parse_prefix",
+    "process",
+    "vardp",
+]
